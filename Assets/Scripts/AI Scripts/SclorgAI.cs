bb1534da2d372e7b8645099173c0e777a89af990
Amadeus Chia <foodpunch@gmail.com>
2016-01-09 16:37:30 +0800

    Added Gulnaz Reflect anim Added Sclorg Model and animations Edited Sclorg Script

--- Assets/Scripts/AI Scripts/SclorgAI.cs
+++ Assets/Scripts/AI Scripts/SclorgAI.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class SclorgAI : AIbase {
 
+	//GameObject player;
+	GameObject target;
 	float _timer;
 	Rigidbody _rb;
 	Transform shootPoint;
 	Rigidbody projectile;
 	float distance;
 	bool shoot;
+	Vector3 targetSpeed;
 	Vector3 futurePos;
 	Vector3 direction;
 	float iterations = 30f;
@@ -41,6 +44,24 @@ public class SclorgAI : AIbase {
 	protected override void PassiveAbility()
 	{
 		//throw new System.NotImplementedException();
+		_timer += Time.deltaTime;
 	}
-	
+	void LookAtPlayer()
+	{
+		target = player.gameObject;
+		targetSpeed = target.GetComponent<Rigidbody>().velocity;
+		futurePos = target.transform.position + (targetSpeed * (distance / iterations));
+		direction = futurePos - transform.position;
+		direction.y = 0;
+		Quaternion lookRot = Quaternion.LookRotation(direction);
+		_rb.MoveRotation(lookRot);
+		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 2f * Time.deltaTime);
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.collider.CompareTag("dagger"))
+		{
+ 			//play hit deflect sound
+		}
+	}
 }
