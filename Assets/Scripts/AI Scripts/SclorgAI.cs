ac1a7b4e53a040146a4959cc239a75d4dca1ff4b
Amadeus Chia <foodpunch@gmail.com>
2016-01-09 14:37:34 +0800

    Cleaned up animator Put in new animations for gulnaz Put in  Mana Fire animation Fixed Gulnaz Roll to allow it to roll whenever you're moving Reorganised some folders Created Sclorg AI script Added Trail Renderer to Nepto Projectile (created a new mat for it too)

---
+++ Assets/Scripts/AI Scripts/SclorgAI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SclorgAI : AIbase {
+
+	float _timer;
+	Rigidbody _rb;
+	Transform shootPoint;
+	Rigidbody projectile;
+	float distance;
+	bool shoot;
+	Vector3 futurePos;
+	Vector3 direction;
+	float iterations = 30f;
+
+	[System.Serializable] 
+	enum SclorgState
+	{
+		PURSUE,
+		SHOOT
+	};
+	SclorgState sclorgStates = SclorgState.PURSUE;
+
+	// Use this for initialization
+	protected override void Start()
+	{
+		base.Start();
+		canPosses = false;
+	}
+	protected override void ActivateAbility()
+	{
+		//throw new System.NotImplementedException();
+		switch (sclorgStates)
+		{ 
+			case SclorgState.PURSUE:
+				break;
+			case SclorgState.SHOOT:
+				break;
+		}
+	}
+	protected override void PassiveAbility()
+	{
+		//throw new System.NotImplementedException();
+	}
+	
+}
