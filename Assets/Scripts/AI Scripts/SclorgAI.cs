93266f63b02e38142cd8fa0526c3715dc37f95ff
Amadeus Chia <foodpunch@gmail.com>
2016-01-14 22:04:29 +0800

    Added Sclorg AI animation transitions Added Sclorg AI logic (Need to fix Sclorg Movement) Added slow effect to gulnaz controller Imported Sclorg projectile

--- Assets/Scripts/AI Scripts/SclorgAI.cs
+++ Assets/Scripts/AI Scripts/SclorgAI.cs
@@ -7,37 +7,83 @@ public class SclorgAI : AIbase {
 	GameObject target;
 	float _timer;
 	Rigidbody _rb;
-	Transform shootPoint;
-	Rigidbody projectile;
-	float distance;
+
+	float _distance;
 	bool shoot;
 	Vector3 targetSpeed;
 	Vector3 futurePos;
-	Vector3 direction;
+	Vector3 _direction;
 	float iterations = 30f;
+	Animator SclorgAnim;
 
-	[System.Serializable] 
+	[SerializeField]
+	Rigidbody projectile;
+	[SerializeField]
+	Transform shootPos;
+
+	[SerializeField] 
 	enum SclorgState
 	{
 		PURSUE,
-		SHOOT
+		SHOOT,
+		DEATH
 	};
-	SclorgState sclorgStates = SclorgState.PURSUE;
+	[SerializeField]SclorgState sclorgStates = SclorgState.PURSUE;
 
 	// Use this for initialization
 	protected override void Start()
 	{
 		base.Start();
 		canPosses = false;
+		health = 999;
+		_rb = gameObject.GetComponent<Rigidbody>();
+		SclorgAnim = gameObject.GetComponent<Animator>();
+	}
+	protected override void Roam()
+	{
+		base.Roam();
+		_distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+		Debug.Log(_distance);
+		if (distance < 35)
+		{
+			AIState = States.pursue;
+		}
 	}
 	protected override void ActivateAbility()
 	{
 		//throw new System.NotImplementedException();
+		currentTargetPosition = player.transform.position;
 		switch (sclorgStates)
 		{ 
 			case SclorgState.PURSUE:
+				SclorgAnim.SetBool("isAttacking", false);
+				SclorgAnim.SetBool("isMoving",true);
+				MoveTowardsTarget();
+				if (distance < 15)
+				{
+					_timer = 0;
+					SclorgAnim.SetBool("isAttacking", true);
+					SclorgAnim.SetBool("isMoving", false);
+					shoot = false;
+					sclorgStates = SclorgState.SHOOT;
+				}
 				break;
 			case SclorgState.SHOOT:
+				SclorgAnim.SetBool("isAttacking", true);
+				SclorgAnim.SetBool("isMoving", false);
+				LookAtPlayer();
+				if (_timer > 2.4f && !shoot && SclorgAnim.GetBool("isAttacking"))
+				{
+					Fire();
+				}
+				if ((_timer >= 3.75f || distance > 15) && SclorgAnim.GetBool("isAttacking"))
+				{
+					SclorgAnim.SetBool("isAttacking", false);
+					SclorgAnim.SetBool("isMoving", true);
+					sclorgStates = SclorgState.PURSUE;
+				}
+				break;
+			case SclorgState.DEATH:
 				break;
 		}
 	}
@@ -51,11 +97,19 @@ public class SclorgAI : AIbase {
 		target = player.gameObject;
 		targetSpeed = target.GetComponent<Rigidbody>().velocity;
 		futurePos = target.transform.position + (targetSpeed * (distance / iterations));
-		direction = futurePos - transform.position;
-		direction.y = 0;
-		Quaternion lookRot = Quaternion.LookRotation(direction);
+		_direction = futurePos - transform.position;
+		_direction.y = 0;
+		Quaternion lookRot = Quaternion.LookRotation(_direction);
 		_rb.MoveRotation(lookRot);
 		//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 2f * Time.deltaTime);
+	}
+	void Fire()
+	{
+		Debug.Log("fire");
+		Rigidbody projectileClone = Instantiate(projectile, shootPos.position, transform.rotation) as Rigidbody;
+		projectileClone.velocity = transform.forward * 20;
+		shoot = true;
+
 	}
 	void OnCollisionEnter(Collision col)
 	{
