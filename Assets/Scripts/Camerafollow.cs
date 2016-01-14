e65dabc3bad4e6724a12b62968a8471b496e1cd5
Darylcxz <dcxz96@live.com>
2016-01-14 17:37:10 +0800

    vase break done

--- Assets/Scripts/Camerafollow.cs
+++ Assets/Scripts/Camerafollow.cs
@@ -4,14 +4,11 @@ using System.Collections;
 public class Camerafollow : MonoBehaviour {
 
 	public static GameObject targetUnit;
-    [SerializeField]private RectTransform foreground;
-    private Camera maincam;
 
 	// Use this for initialization
 	void Start () {
 
 		targetUnit = GameObject.Find ("Character");
-        maincam = gameObject.GetComponentInChildren<Camera>();
 	
 	}
 	
@@ -24,7 +21,6 @@ public class Camerafollow : MonoBehaviour {
 		Vector3 currLocation = new Vector3 (transform.position.x + moveX / 6, transform.position.y + moveY / 6, transform.position.z + moveZ / 6);
 		transform.position = currLocation;
 
-        Vector3 oppdir = transform.position * -1;
-        foreground.anchoredPosition = maincam.WorldToScreenPoint(oppdir);
+        
 	}
 }
---
+++ Assets/Scripts/VaseBreak.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VaseBreak : MonoBehaviour {
+    private Rigidbody[] pieces;
+    private BoxCollider thisthing;
+	// Use this for initialization
+	void Start () {
+
+        thisthing = gameObject.GetComponent<BoxCollider>();
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.collider.CompareTag("dagger"))
+        {
+            Debug.Log("hit");
+            thisthing.enabled = false;
+            pieces = gameObject.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody piece in pieces)
+            {
+                piece.isKinematic = false;
+            }
+            foreach (Rigidbody piece in pieces)
+            {
+                piece.AddExplosionForce(50,transform.position, 10);
+            }
+            
+        }
+    }
+}
