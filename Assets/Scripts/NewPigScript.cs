75f777516f8c9b44e1af7d9865140684477678b4
Amadeus Chia <foodpunch@gmail.com>
2016-02-11 12:55:51 +0800

    Organised 1-1 Fixed AI to be able to be possessed by Gulnaz again Defined a small area for the pig to roam in 1-1

--- Assets/Scripts/NewPigScript.cs
+++ Assets/Scripts/NewPigScript.cs
@@ -2,14 +2,69 @@
 using System.Collections;
 
 public class NewPigScript : NewAIBA {
+	[SerializeField]ParticleSystem pigExplosion; //particle system for pig sneeze
+	Animator pigAnim;
+	[SerializeField]AudioClip sneezeSound;
+	[SerializeField]AudioClip squealSound;
+	AudioSource pigAudioSource;
+	bool hasPlayed;
+
+	protected override void Start()
+	{
+		base.Start();
+		pigAnim = GetComponent<Animator>();
+		pigAudioSource = GetComponent<AudioSource>();
+
+	}
+
 
 	protected override void ActivateAbility()
 	{
-		//throw new System.NotImplementedException();
+		if(!pigExplosion.isPlaying)
+		{
+			hasPlayed = false;
+			pigExplosion.Play();
+			if(!pigAudioSource.isPlaying && !hasPlayed)
+			{
+				StartCoroutine(PlaySound(sneezeSound));
+			}
+			StartCoroutine(PlaySound(sneezeSound));
+			pigAnim.SetTrigger("tSneeze");
+		}
 	}
 	protected override void PassiveAbility()
 	{
-		//throw new System.NotImplementedException();
+		//Plays pig anims
+		switch(AIState)
+		{
+			case StateMachine.WALK:
+				hasPlayed = false;
+				pigAnim.SetBool("isMoving", true);
+				break;
+			case StateMachine.IDLE:
+				hasPlayed = false;
+				pigAnim.SetBool("isMoving", false);
+				break;
+			case StateMachine.POSSESSED:
+				if(!pigAudioSource.isPlaying && !hasPlayed)
+				{
+					StartCoroutine(PlaySound(squealSound));
+				}
+				pigAnim.SetBool("isMoving", false);
+				break;
+			case StateMachine.WAIT:
+				pigAnim.SetBool("isMoving", false);
+				break;
+
+		}
+	}
+	IEnumerator PlaySound(AudioClip _sound)
+	{
+		pigAudioSource.clip = _sound;
+		pigAudioSource.PlayOneShot(_sound);
+		hasPlayed = true;
+
+		yield return new WaitForEndOfFrame();
 	}
 
 }
