68ad7f928a621294c971d0f24ed8c0bffe330d6f
RitzOE <ritzann@hotmail.com>
2016-02-15 16:38:12 +0800

    Removed useless Arrow model Edited scenes, configured somewhat working camera pan sequence (fix angles) Trying to implement bullet time

--- Assets/Scripts/NewTutorialController.cs
+++ Assets/Scripts/NewTutorialController.cs
@@ -18,24 +18,35 @@ public class NewTutorialController : MonoBehaviour {
 		POSSESS,
 		SERIKPIG
 	};
-	[SerializeField]
-	Part DialoguePart;
-	string _dialogueName;
-	bool textPlay;
+	[SerializeField] Part DialoguePart;
+    [SerializeField] List<Transform> CamPositions = new List<Transform>();
+    
+
+    string _dialogueName;
+	bool textPlay = true;
     bool statechange;
     [SerializeField]
     Transform _camera;
+    Quaternion _orignalRot;
+    bool panCam;
+    int _panIndex;
+
+    //Camera panning values
+    float _timer;
+    [SerializeField] int _speedMultiplier = 1;
 	// Use this for initialization
 	void Start () {
 		_dScript = _dScript.GetComponent<DialogueScript>();
         _camera = _camera.GetComponent<Transform>();
-	}
+        _dScript.hasDialogueEnd = true;
+        _orignalRot = _camera.transform.rotation;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		DialogueStateMachine();
-
-	}
+        Debug.Log(DialogueScript._seqNum);
+    }
 	void DialogueStateMachine()
 	{
 		switch (DialoguePart)
@@ -47,11 +58,22 @@ public class NewTutorialController : MonoBehaviour {
 				break;
 			case Part.DESH:
 				_dialogueName = "desh1-1";
-                //
                 ChangeState(Part.SHAKE);
                 break;
 			case Part.SHAKE:
-				_dialogueName = "Shake1-1";
+                if (DialogueScript._seqNum == 3 && !panCam)
+                {
+                    _panIndex = 0;
+                    _timer = 0;
+                    panCam = true;
+                }
+                if (DialogueScript._seqNum == 4)
+                {
+                    panCam = false;
+                    _camera.GetComponent<Camerafollow>().enabled = true;
+                    _camera.transform.rotation = _orignalRot;
+                }
+                _dialogueName = "Shake1-1";
                 ChangeState(Part.DEFEAT);
                 break;
 			case Part.DEFEAT:
@@ -84,6 +106,18 @@ public class NewTutorialController : MonoBehaviour {
 			PlayText(_dialogueName);
             textPlay = true;
 		}
+        if(panCam)
+        {
+            _timer += Time.deltaTime;
+            _camera.position = Vector3.Lerp(transform.localPosition, CamPositions[_panIndex].position, _timer * _speedMultiplier);
+            _camera.rotation = Quaternion.Slerp(transform.localRotation, CamPositions[_panIndex].rotation, _timer * _speedMultiplier);
+            if (_timer > 1)
+            {
+                _timer = 1;
+             //   panCam = false;
+            }
+        }
+        
 	}
    public void DialogueEnd()
     {
