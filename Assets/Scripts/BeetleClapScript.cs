ced481b58fa5212ebce4e83046ae470ab6529899
Darylcxz <dcxz96@live.com>
2016-02-20 01:32:38 +0800

    VillageScene is supposedly complete but BGM sometimes doesnt play..?

---
+++ Assets/Scripts/BeetleClapScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeetleClapScript : MonoBehaviour {
+    [SerializeField]AudioClip clap;
+    AudioSource beetle;
+	// Use this for initialization
+	void Start () {
+        beetle = gameObject.GetComponent<AudioSource>();
+	}
+
+    public void Clap()
+    {
+        beetle.PlayOneShot(clap);
+        Debug.Log("Clap!");
+    }
+}
