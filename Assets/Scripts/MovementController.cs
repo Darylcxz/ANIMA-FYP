132d2dc07a0e80a5a0ef17e0ce3b053a6d9735d2
RitzOE <ritzann@hotmail.com>
2016-02-19 17:59:00 +0800

    Fixed 95% of 1-1 Implemented most of the dialogue for 1-1 Fixed sequence breaking shenanigans in 1-1 Fixed a fuck ton of bugs

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -318,6 +318,8 @@ public class MovementController : MonoBehaviour {
             _rigidBody.velocity = Vector3.zero;
             charStates = States.idle;
             _dagger.GetComponent<TrailRenderer>().enabled = false;
+            _dagger.enabled = false;
+            
         }
 		if (bKeyboard && !bForcedMove)
 		{
--- Assets/Scripts/VineBreak.cs
+++ Assets/Scripts/VineBreak.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class VineBreak : MonoBehaviour {
 
     [SerializeField] Rigidbody[] _rigidBodies;
+    [SerializeField] GameObject _optionalTrigger;
 
 	// Use this for initialization
 	void Start () {
@@ -19,39 +20,13 @@ public class VineBreak : MonoBehaviour {
                 Destroy(_rb.gameObject, 3.5f);
             }
             gameObject.GetComponent<Collider>().enabled = false;
-
-			//gameObject.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(1).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(2).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(3).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(4).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(5).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(6).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(7).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(8).GetComponent<Rigidbody>().isKinematic = false;
-			//gameObject.transform.GetChild(9).GetComponent<Rigidbody>().isKinematic = false;
-			
-			//Destroy(gameObject.transform.GetChild(0).gameObject,3.5f);
-			//Destroy(gameObject.transform.GetChild(1).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(2).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(3).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(4).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(5).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(6).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(7).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(8).gameObject, 3.5f);
-			//Destroy(gameObject.transform.GetChild(9).gameObject, 3.5f);
-
-			//gameObject.GetComponent<Collider>().enabled = false;
-
-			if (gameObject.name.Contains("Wheel"))
-			{
-				GameObject.FindGameObjectWithTag("WaterWheel").GetComponent<Animator>().SetBool("bWheel", true);
-				GameObject.FindGameObjectWithTag("Saw").GetComponent<Animator>().SetBool("bWheel", true);
-			}
-			//gameObject.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = true;
-			//Destroy(gameObject,0.1f);
-
+            //gameObject.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = true;
+            //Destroy(gameObject,0.1f);
+            if(_optionalTrigger!=null)
+            {
+                _optionalTrigger.SendMessage("OnVineBreak", SendMessageOptions.DontRequireReceiver);
+            }
 		}
 	}
+
 }
