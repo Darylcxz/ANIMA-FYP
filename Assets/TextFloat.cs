19a290a69da9ac702f80f4421efc7bf2941ec18a
RitzOE <ritzann@hotmail.com>
2016-02-22 13:25:25 +0800

    Fixed Desh mostly, Added Exclamation marks to Desh, Added new tags/layer for desh Exposed some variables in AI Base, Made UI floaty in 1-1

---
+++ Assets/TextFloat.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextFloat : MonoBehaviour {
+    float gameTime;
+    Vector3 orignalPos;
+	// Use this for initialization
+	void Start () {
+        orignalPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        gameTime += Time.deltaTime;
+        Vector3 newPos = new Vector3(transform.position.x, (Mathf.PingPong(gameTime / 2, orignalPos.y+50 - orignalPos.y-50) + orignalPos.y-50), transform.position.z);
+        transform.position = newPos;
+        Debug.Log(transform.position);
+        Debug.Log(gameTime);
+	}
+}
